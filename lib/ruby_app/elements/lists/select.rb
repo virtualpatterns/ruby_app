6530f828e46066e4121be2e63eef0c82f82f7f00
Frank G. Ficnar <frank.ficnar@gmail.com>
2011-11-21 16:54:21 -0500

    Modified 'selected_X' of RubyApp::Elements::Lists::Select

--- lib/ruby_app/elements/lists/select.rb
+++ lib/ruby_app/elements/lists/select.rb
@@ -11,26 +11,19 @@ module RubyApp
 
         exclude_parent_template(:html, :css, :js)
 
-        attr_accessor :selected_index, :selected_item
+        attr_accessor :selected_item
 
         def initialize
           super
         end
 
-        def selected_index=(index)
-          @selected_index = index
-          @selected_item = @items[index]
-        end
-
         def selected_item=(item)
-          @selected_index = @items.index(item)
-          @selected_item = @selected_index ? @items[@selected_index] : nil
+          @selected_item = item
         end
 
         protected
 
           def on_clicked(event)
-            @selected_index = event.index
             @selected_item = event.item
             event.update_element(self)
             super(event)
