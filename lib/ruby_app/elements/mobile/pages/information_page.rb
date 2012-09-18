ce2065786e495725c16447fb6d8e241322b71c4f
Frank G. Ficnar <frank.ficnar@gmail.com>
2012-09-18 11:06:26 -0400

    Adding cookies to information page

--- lib/ruby_app/elements/mobile/pages/information_page.rb
+++ lib/ruby_app/elements/mobile/pages/information_page.rb
@@ -9,6 +9,7 @@ module RubyApp
         require 'ruby_app/elements/mobile/navigation/navigation_link'
         require 'ruby_app/elements/mobile/page'
         require 'ruby_app/elements/mobile/pages/information/configuration_page'
+        require 'ruby_app/elements/mobile/pages/information/cookies_page'
         require 'ruby_app/elements/mobile/pages/information/environment_page'
         require 'ruby_app/elements/mobile/pages/information/sessions_page'
         require 'ruby_app/elements/mobile/pages/information/scripts_page'
@@ -27,6 +28,11 @@ module RubyApp
             @environment_link = RubyApp::Elements::Mobile::Navigation::NavigationLink.new
             @environment_link.clicked do |element, event|
               RubyApp::Elements::Mobile::Pages::Information::EnvironmentPage.new.show(event, element.options)
+            end
+
+            @cookies_link = RubyApp::Elements::Mobile::Navigation::NavigationLink.new
+            @cookies_link.clicked do |element, event|
+              RubyApp::Elements::Mobile::Pages::Information::CookiesPage.new.show(event, element.options)
             end
 
             @configuration_link = RubyApp::Elements::Mobile::Navigation::NavigationLink.new
