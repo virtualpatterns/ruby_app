a44567e9519a52f490ece395030be9c903fbdf6c
Frank G. Ficnar <frank.ficnar@gmail.com>
2012-10-21 01:51:20 -0400

    Uncommenting tests

--- lib/ruby_app/scripts/elements/lists.rb
+++ lib/ruby_app/scripts/elements/lists.rb
@@ -9,27 +9,27 @@ add_step! (RubyApp::Elements::Mobile::Page::ShownEvent) { |event| event.assert_e
 add_step! (RubyApp::Element::AssertedEvent)             { |event| event.assert_exists_link('OK') }
 add_step! (RubyApp::Element::AssertedEvent)             { |event| event.click_link('OK') }
 
-# add_step! (RubyApp::Elements::Mobile::Page::ShownEvent) { |event| event.assert_exists_link('Item 01') }
-# add_step! (RubyApp::Element::AssertedEvent)             { |event| event.click_list_item('Item 01') }
-# add_step! (RubyApp::Elements::Mobile::Page::ShownEvent) { |event| event.assert_exists_text('You clicked the item Item 01.') }
-# add_step! (RubyApp::Element::AssertedEvent)             { |event| event.assert_exists_link('OK') }
-# add_step! (RubyApp::Element::AssertedEvent)             { |event| event.click_link('OK') }
+add_step! (RubyApp::Elements::Mobile::Page::ShownEvent) { |event| event.assert_exists_link('Item 01') }
+add_step! (RubyApp::Element::AssertedEvent)             { |event| event.click_list_item('Item 01') }
+add_step! (RubyApp::Elements::Mobile::Page::ShownEvent) { |event| event.assert_exists_text('You clicked the item Item 01.') }
+add_step! (RubyApp::Element::AssertedEvent)             { |event| event.assert_exists_link('OK') }
+add_step! (RubyApp::Element::AssertedEvent)             { |event| event.click_link('OK') }
 
-# add_step! (RubyApp::Elements::Mobile::Page::ShownEvent) { |event| event.assert_not_exists_link('Item 04') }
+add_step! (RubyApp::Elements::Mobile::Page::ShownEvent) { |event| event.assert_not_exists_link('Item 04') }
 
-# add_step! (RubyApp::Element::AssertedEvent)             { |event| event.assert_exists_link('Item 05') }
-# add_step! (RubyApp::Element::AssertedEvent)             { |event| event.click_list_item('Item 05') }
-# add_step! (RubyApp::Elements::Mobile::Page::ShownEvent) { |event| event.assert_exists_text('You clicked the item Item 05.') }
-# add_step! (RubyApp::Element::AssertedEvent)             { |event| event.assert_exists_link('OK') }
-# add_step! (RubyApp::Element::AssertedEvent)             { |event| event.click_link('OK') }
-# add_step! (RubyApp::Elements::Mobile::Page::ShownEvent) { |event| event.assert_exists_link('Item 05') }
-# add_step! (RubyApp::Element::AssertedEvent)             { |event| event.click_list_link('Item 05') }
-# add_step! (RubyApp::Elements::Mobile::Page::ShownEvent) { |event| event.assert_exists_text('You clicked the link Item 05.') }
-# add_step! (RubyApp::Element::AssertedEvent)             { |event| event.assert_exists_link('OK') }
-# add_step! (RubyApp::Element::AssertedEvent)             { |event| event.click_link('OK') }
+add_step! (RubyApp::Element::AssertedEvent)             { |event| event.assert_exists_link('Item 05') }
+add_step! (RubyApp::Element::AssertedEvent)             { |event| event.click_list_item('Item 05') }
+add_step! (RubyApp::Elements::Mobile::Page::ShownEvent) { |event| event.assert_exists_text('You clicked the item Item 05.') }
+add_step! (RubyApp::Element::AssertedEvent)             { |event| event.assert_exists_link('OK') }
+add_step! (RubyApp::Element::AssertedEvent)             { |event| event.click_link('OK') }
+add_step! (RubyApp::Elements::Mobile::Page::ShownEvent) { |event| event.assert_exists_link('Item 05') }
+add_step! (RubyApp::Element::AssertedEvent)             { |event| event.click_list_link('Item 05') }
+add_step! (RubyApp::Elements::Mobile::Page::ShownEvent) { |event| event.assert_exists_text('You clicked the link Item 05.') }
+add_step! (RubyApp::Element::AssertedEvent)             { |event| event.assert_exists_link('OK') }
+add_step! (RubyApp::Element::AssertedEvent)             { |event| event.click_link('OK') }
 
-# add_step! (RubyApp::Elements::Mobile::Page::ShownEvent) { |event| event.assert_not_exists_link('Item 08') }
+add_step! (RubyApp::Elements::Mobile::Page::ShownEvent) { |event| event.assert_not_exists_link('Item 08') }
 
-# add_step! (RubyApp::Element::AssertedEvent)             { |event| event.click_link('Back') }
-# add_step! (RubyApp::Elements::Mobile::Page::ShownEvent) { |event| event.click_link('Back') }
-# add_step! (RubyApp::Elements::Mobile::Page::ShownEvent) { |event| event.execute {} }
+add_step! (RubyApp::Element::AssertedEvent)             { |event| event.click_link('Back') }
+add_step! (RubyApp::Elements::Mobile::Page::ShownEvent) { |event| event.click_link('Back') }
+add_step! (RubyApp::Elements::Mobile::Page::ShownEvent) { |event| event.execute {} }
