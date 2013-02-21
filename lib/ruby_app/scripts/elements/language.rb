00b8cac2dbe27762b083a6b97d899520b9c1fcf0
virtualpatterns <frank.ficnar@gmail.com>
2013-02-21 16:17:53 -0500

    Fixing script to use root_or_nil instead of just root

--- lib/ruby_app/scripts/elements/language.rb
+++ lib/ruby_app/scripts/elements/language.rb
@@ -2,9 +2,9 @@ load_script! 'common/features'
 
 add_step! (RubyApp::Element::Event)                       { |event| event.assert_exists_link('Language') }
 add_step! (RubyApp::Element::AssertedEvent)               { |event| event.click_link('Language') }
-add_step! (RubyApp::Elements::Mobile::Page::ShownEvent)   { |event| event.go("#{RubyApp::Application.root}/en") }
+add_step! (RubyApp::Elements::Mobile::Page::ShownEvent)   { |event| event.go("#{RubyApp::Application.root_or_nil}/en") }
 add_step! (RubyApp::Elements::Mobile::Page::LoadedEvent)  { |event| event.assert_exists_link('Back') }
-add_step! (RubyApp::Element::AssertedEvent)               { |event| event.go("#{RubyApp::Application.root}/fr") }
+add_step! (RubyApp::Element::AssertedEvent)               { |event| event.go("#{RubyApp::Application.root_or_nil}/fr") }
 add_step! (RubyApp::Elements::Mobile::Page::LoadedEvent)  { |event| event.assert_exists_link('Reculer') }
 add_step! (RubyApp::Element::AssertedEvent)               { |event| event.go(RubyApp::Application.root) }
 add_step! (RubyApp::Elements::Mobile::Page::LoadedEvent)  { |event| event.click_link('Back') }
